c5e44cb915159fc0ae7e0cd0ad2d295898460f48
Hamza Elhamdadi <irishlad98@1x-nat-vl930-172-30-65-91.wireless.umass.edu>
2021-11-23 20:22:46 -0500

    added CV

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import Col from 'react-bootstrap/Col';
 
 import Card from 'react-bootstrap/Card'
 
-import MailTo from './components/MailTo';
-
 //Navbar imports
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -19,8 +17,6 @@ import { Timeline, Follow } from 'react-twitter-widgets';
 //Styles imports
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
-
 
 function App() {
   return (
@@ -34,54 +30,35 @@ function App() {
         </Nav>
       </Navbar>
       <div className="App">
-          <Container className='mx-auto'>
+          <Container fluid className='mx-auto' style={{margin: '1rem 3rem 0 3rem'}}>
             <Row>
-              <Col xs={10} lg={4} className='text-left mx-auto'>
-              <Card className='cropped'>
-                <Card.Img src={'/IMG_7839_Original.jpg'} alt="Hamza Elhamdadi" />
-                <Card.Body className='bckgrnd cardbodytext'>
-                    <h1>Hamza Elhamdadi</h1>
-                    <p>HCI-VIS Lab - Department of Computer Science</p>
-                    <p>University of Massachusetts Amherst</p>
-                    <MailTo>
-                      Email me: helhamdadi 'at' umass.edu
-                    </MailTo>
-                </Card.Body>
-              </Card>
+              <Col xs={12} sm={12} md={6} lg={4} xl={4} className='text-center mx-auto'>
+                <div className='cropped mx-auto'>
+                  <img src={'/IMG_7839_Original.jpg'} className='profile' alt="Hamza Elhamdadi"></img>
+                </div>
+                <div className='bckgrnd cardbodytext mx-auto'>
+                <h1>Hamza Elhamdadi</h1>
+                </div>
               </Col>
-              <Col xs={10} lg={8} className='mx-auto' >
+              <Col xs={12} sm={12} md={6} lg={5} xl={4} className='mx-auto' >
                 <Row style={{marginTop: '10px', color: '#7050a0'}}>
-                  <Col lg={6} className='text-left mx-auto'>
+                  <Col lg={12} className='text-left mx-auto'>
                   <h2>About Me</h2>
-                  <p>
+                  <p style={{marginBottom: 10}}>
                     I am a doctoral student in the <a style={{'margin': 0}} target='_' href='https://groups.cs.umass.edu/hci-vis/'> HCI-Vis Lab</a>&nbsp;
                     at the <a style={{'margin': 0}} target="_" href="https://www.umass.edu"> University of Massachusetts Amherst</a>. I joined UMass
                     in the Fall of 2021. I received my Master's and Bachelor's degrees from the Computer Science and Engineering
                     at the <a style={{'margin': 0}} target='_' href='https://usf.edu'>University of South Florida</a>.
                   </p>
-                  <p>
+                  <p style={{marginBottom: '10%'}}>
                     I am currently pursuing a Ph.D. under the advising of <a style={{'margin': 0}} target='_' href='https://cyxiong.com'>Dr. Cindy Xiong</a>.
                     My research interests in the area of Data & Information Visualization include Perception in Visualization and
                     Topological Data Analysis. My recent published work applies Topological Data Analysis to Emotion Detection in the field of Affective Computing.
                   </p>
                   </Col>
-                  <Col lg={6} className='mx-auto'>
-                  <Timeline
-                      dataSource={{
-                        sourceType: 'profile',
-                        screenName: 'ElhamdadiHamza'
-                      }}
-                      options={{
-                        height: '380',
-                        chrome: "noheader, nofooter"
-                      }}
-                    />
-                  <Follow username="ElhamdadiHamza" options={{ size: "small" }} />
-                  </Col>
                 </Row>
                 <Row className='text-left mx-auto' style={{color: '#7050a0'}}>
-                  <Card>
-                    <Card.Body className='bckgrnd'>
+                  <Col className='bckgrnd cardbodytext' style={{marginBottom: '10%'}} xs={12} sm={12} md={12} lg={12} xl={12}>
                     <h4>Recent News</h4>
                       <ul className='table-list'>
                         <li className='table-element'>
@@ -89,19 +66,32 @@ function App() {
                         </li>
                         <li className='table-element element-right'><b>Fall 2021</b></li>
                       </ul>
-                      <a href='https://www.youtube.com/watch?v=buqJx0w5PiQ' target='_' style={{textAlign: 'right'}}>Watch my IEEE Vis Presentation here!</a>
+                      <ul className='table-list'>
+                        <li className='table-element'>
+                        <a href='https://www.youtube.com/watch?v=buqJx0w5PiQ' target='_' style={{textAlign: 'right'}}>Watch my IEEE Vis Presentation here!</a>
+                        </li> 
+                      </ul>
+                      
                       <ul className='table-list'>
                         <li className='table-element'><p>Accepted to Ph.D. program at UMass Amherst</p></li>
                         <li className='table-element element-right'><b>Fall 2021</b></li>
                       </ul>
-                      <ul className='table-list'>
-                        <li className='table-element'><p>Master of Science Awarded at USF</p></li>
-                        <li className='table-element element-right'><b>Summer 2021</b></li>
-                      </ul> 
-                    </Card.Body>
-                  </Card>
-                      
+                  </Col>
                 </Row>
+              </Col>
+              <Col xs={12} sm={12} md={12} lg={3} xl={4}>
+                  <Timeline
+                            dataSource={{
+                              sourceType: 'profile',
+                              screenName: 'ElhamdadiHamza'
+                            }}
+                            options={{
+                              height: '80vh',
+                              chrome: "noheader, nofooter"
+                            }}
+                          />
+                  <Follow username="ElhamdadiHamza" options={{ size: "small" }} />
+                  
               </Col>
             </Row>
           </Container>
